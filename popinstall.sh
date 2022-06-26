8ae2a1bf1ee1e7acd6f1f96b455ad07ab78d3b7a
MishalAbdullah <85678545+Mishalabdullah@users.noreply.github.com>
2022-06-26 09:53:55 +0530

    Create popinstall.sh

---
+++ popinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt install jupyter-notebook
+sudo apt install mpv
+sudo apt install keepassxe
+sudo apt install qbittorrent
+
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.nextcloud.desktopclient.nextcloud
+flatpak install flathub com.github.alecaddd.sequeler
+flatpak install flathub com.github.johnfactotum.Foliate
+flatpak install flathub io.posidon.Paper
+flatpak install flathub com.github.micahflee.torbrowser-launcher
+flatpak install flathub com.simplenote.Simplenote
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub org.signal.Signal
