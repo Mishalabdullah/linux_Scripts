b075fb75c5b98320228e1fd0312a06bcaccf3d0a
MishalAbdullah <85678545+Mishalabdullah@users.noreply.github.com>
2022-09-04 17:09:23 +0530

    Update popinstall.sh

--- popinstall.sh
+++ popinstall.sh
@@ -7,9 +7,8 @@ sudo apt install brave-browser
 
 flatpak install flathub md.obsidian.Obsidian
 flatpak install flathub com.nextcloud.desktopclient.nextcloud
-flatpak install flathub com.github.alecaddd.sequeler
+flatpak install flathub nz.mega.MEGAsync
 flatpak install flathub com.github.johnfactotum.Foliate
-flatpak install flathub io.posidon.Paper
 flatpak install flathub com.github.micahflee.torbrowser-launcher
 flatpak install flathub com.simplenote.Simplenote
 flatpak install flathub org.gimp.GIMP
